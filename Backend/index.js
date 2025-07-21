05901403d4ac552681fbc0e9e5dc599ae570e0b4
khunjuliwe <147825634+zakzak45@users.noreply.github.com>
2025-07-21 20:07:29 +0000

    complete backend

--- Backend/index.js
+++ Backend/index.js
@@ -4,26 +4,14 @@ const nodemailer =require('nodemailer')
 const randomatic =require('randomatic')
 const body_parser =require('body-parser')
 const cors = require('cors')
-
+const app =express()
 
 require('dotenv').config()
 
 app.use(body_parser.json())
 app.use(cors())
-const app =express()
+
 const PORT = process.env.PORT || 4000
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 mongoose.connect('mongodb+srv://zayne:khunjuliwe@cluster0.01xvdjq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(()=>{
@@ -66,6 +54,65 @@ async function sendOtpEmail(email,otp) {
 } 
 }
 
+app.post('/auth/login', async (req, res) => {
+    const { email, password } = req.body;
+    console.log(req.body)
+
+    try {
+        const user = await User.findOne({ email, password });
+        console.log(user)
+        if (!user) {
+            return res.json(
+                {
+                    success: false,
+                    message: 'Invalid credentials'
+                }
+            );
+        }
+
+        const generatedOtp = randomize('0', 6);
+        user.otp = generatedOtp;
+        await user.save();
+
+        sendOtpEmail(email, generatedOtp);
+
+        return res.json({ success: true });
+    } catch (error) {
+        console.error('Error during login:', error.message);
+        return res.status(500).json(
+            {
+                success: false,
+                message: 'An error occurred during login'
+            }
+        );
+    }
+});
+
+app.post('/auth/verify-otp', async (req, res) => {
+    const { otp } = req.body;
+
+    try {
+        const user = await User.findOne({ otp });
+
+        if (!user) {
+            return res.json({ success: false, message: 'Invalid OTP' });
+        }
+
+        user.otp = '';
+        await user.save();
+
+        return res.json({ success: true });
+    } catch (error) {
+        console.error('Error during OTP verification:', error.message);
+        return res.status(500)
+            .json(
+                {
+                    success: false,
+                    message: 'An error occurred during OTP verification'
+                }
+            );
+    }
+});
 
 
 app.listen(PORT,()=>{
